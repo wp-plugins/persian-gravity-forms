79196358e6abf4d4966f85100b50a9b3f9cc7557
hannanstd <hannanstd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-02 21:50:14 +0000

    up to 1.3.1
    git-svn-id: https://plugins.svn.wordpress.org/persian-gravity-forms/tags/1.3.1@1081189 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/Post_Content_Merge_Tags.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * Persian Gravity Forms // Gravity Forms Post Content Merge Tags
+ *
+ * Adds support for using Gravity Form merge tags in your post content. This functionality requires that the entry ID is
+ * is passed to the post via the "id" parameter.
+ *
+ * Setup your confirmation page (requires GFv1.8) or confirmation URL "Redirect Query String" setting to
+ * include this parameter: 'id={entry_id}'. You can then use any entry-based merge tag in your post content.
+ *
+ * @version   1.2
+ * @author    HANNAN Std <david@gravitywiz.com>
+ * @license   GPL-2.0+
+ * @link      http://gravityforms.ir
+ * @video     http://gravityforms.ir
+ * @copyright 2014 Persian Gravity Forms
+ */
+class PersianGravityForms_Post_Content_Merge_Tags {
+	public static $_entry = null;
+	private static $instance = null;
+    public static function get_instance( $args = array() ) {
+        if( self::$instance == null )
+            self::$instance = new self( $args );
+        return self::$instance;
+    }
+    function __construct( $args ) {
+        if( ! class_exists( 'GFForms' ) )
+            return;
+        $this->_args = wp_parse_args( $args, array(
+            'auto_append_id' => true, // true, false or array of form IDs
+            'encrypt_id'     => false,
+        ) );
+        add_filter( 'the_content', array( $this, 'replace_merge_tags' ), 1 );
+        add_filter( 'gform_replace_merge_tags', array( $this, 'replace_encrypt_entry_id_merge_tag' ), 10, 3 );
+        if( ! empty( $this->_args['auto_append_id'] ) ) {
+            add_filter( 'gform_confirmation', array( $this, 'append_id_parameter' ), 20, 3 );
+        }
+    }
+    function replace_merge_tags( $post_content ) {
+		$wp_session = WP_Session::get_instance();
+		@session_start();
+		if ($wp_session['refid'])
+			$session = $wp_session['refid'];
+		else 
+			$session = $_SESSION["refid"];
+        $entry = $this->get_entry();
+        if( !$entry )
+        return $post_content;
+        $form = GFFormsModel::get_form_meta( $entry['form_id'] );
+		if ( $session == $form["id"].$entry["id"] ) {
+        $post_content = $this->replace_field_label_merge_tags( $post_content, $form );
+        $post_content = GFCommon::replace_variables( $post_content, $form, $entry, false, false, false );
+		} 
+		return $post_content;
+    }
+    function replace_field_label_merge_tags( $text, $form ) {
+        preg_match_all( '/{([^:]+?)}/', $text, $matches, PREG_SET_ORDER );
+        if( empty( $matches ) )
+            return $text;
+        foreach( $matches as $match ) {
+            list( $search, $field_label ) = $match;
+            foreach( $form['fields'] as $field ) {
+                $full_input_id = false;
+                $matches_admin_label = rgar( $field, 'adminLabel' ) == $field_label;
+                $matches_field_label = false;
+                if( is_array( $field['inputs'] ) ) {
+                    foreach( $field['inputs'] as $input ) {
+                        if( GFFormsModel::get_label( $field, $input['id'] ) == $field_label ) {
+                            $matches_field_label = true;
+                            $input_id = $input['id'];
+                            break;
+                        }
+                    }
+                } else {
+                    $matches_field_label = GFFormsModel::get_label( $field ) == $field_label;
+                    $input_id = $field['id'];
+                }
+                if( ! $matches_admin_label && ! $matches_field_label )
+                    continue;
+                $replace = sprintf( '{%s:%s}', $field_label, (string) $input_id );
+                $text = str_replace( $search, $replace, $text );
+                break;
+            }
+        }
+        return $text;
+    }
+    function replace_encrypt_entry_id_merge_tag( $text, $form, $entry ) {
+        if( strpos( $text, '{encrypted_entry_id}' ) === false ) {
+            return $text;
+        }
+        // $entry is not always a "full" entry
+        $entry_id = rgar( $entry, 'id' );
+        if( $entry_id ) {
+            $entry_id = $this->prepare_id( $entry['id'], true );
+        }
+        return str_replace( '{encrypted_entry_id}', $entry_id, $text );
+    }
+    function append_id_parameter( $confirmation, $form, $entry ) {
+        $is_ajax_redirect = is_string( $confirmation ) && strpos( $confirmation, 'gformRedirect' );
+        $is_redirect      = is_array( $confirmation ) && isset( $confirmation['redirect'] );
+        if( ! $this->is_auto_id_enabled( $form ) || ! ( $is_ajax_redirect || $is_redirect ) ) {
+            return $confirmation;
+        }
+        $id = $this->prepare_id( $entry['id'] );
+        if( $is_ajax_redirect ) {
+            preg_match_all( '/gformRedirect.+?(http.+?)(?=\'|")/', $confirmation, $matches, PREG_SET_ORDER );
+            list( $full_match, $url ) = $matches[0];
+            $redirect_url = add_query_arg( array( 'id' => $id ), $url );
+            $confirmation = str_replace( $url, $redirect_url, $confirmation );
+        } else {
+            $redirect_url             = add_query_arg( array( 'id' => $id ), $confirmation['redirect'] );
+            $confirmation['redirect'] = $redirect_url;
+        }
+
+        return $confirmation;
+    }
+    function prepare_id( $entry_id, $force_encrypt = false ) {
+        $id = $entry_id;
+        $do_encrypt = $force_encrypt || $this->_args['encrypt_id'];
+        if( $do_encrypt && is_callable( array( 'GFCommon', 'encrypt' ) ) ) {
+            $id = rawurlencode( GFCommon::encrypt( $id ) );
+        }
+        return $id;
+    }
+    function get_entry() {
+        if( ! self::$_entry ) {
+            $entry_id = $this->get_entry_id();
+            if( ! $entry_id )
+                return false;
+            $entry = GFFormsModel::get_lead( $entry_id );
+            if( empty( $entry ) )
+                return false;
+            self::$_entry = $entry;
+        }
+        return self::$_entry;
+    }
+    function get_entry_id() {
+        $entry_id = rgget( 'id' );
+        if( $entry_id ) {
+            return $this->maybe_decrypt_entry_id( $entry_id );
+        }
+        $post = get_post();
+        if( $post ) {
+            $entry_id = get_post_meta( $post->ID, '_gform-entry-id', true );
+        }
+        return $entry_id ? $entry_id : false;
+    }
+    function maybe_decrypt_entry_id( $entry_id ) {
+	    $do_encrypt = $this->_args['encrypt_id'];
+        if( ! $entry_id ) {
+            return null;
+        } else if( ! $do_encrypt && is_numeric( $entry_id ) && intval( $entry_id ) > 0 ) {
+            return $entry_id;
+        } else {
+            $entry_id = is_callable( array( 'GFCommon', 'decrypt' ) ) ? GFCommon::decrypt( $entry_id ) : $entry_id;
+            return intval( $entry_id );
+        }
+    }
+    function is_auto_id_enabled( $form ) {
+        $auto_append_id = $this->_args['auto_append_id'];
+        if( is_bool( $auto_append_id ) && $auto_append_id === true )
+            return true;
+        if( is_array( $auto_append_id ) && in_array( $form['id'], $auto_append_id ) )
+            return true;
+        return false;
+    }
+}
+function persiangravityforms_post_content_merge_tags( $args = array() ) {
+return PersianGravityForms_Post_Content_Merge_Tags::get_instance( $args );
+}
+persiangravityforms_post_content_merge_tags();
---
+++ readme.txt
@@ -0,0 +1,156 @@
+﻿=== Persian Gravity Forms ===
+Contributors: hannanstd
+Tags: gravityforms,gravity forms,persian gravity forms,persian gravityforms,gravityforms farsi,gravity forms farsi,rtl,fa_ir,iranian,iranian rials,iranian toman,iran state,gravityforms.ir,gravity forms payment,gravity forms gateway,gravity forms iran gateways
+Donate link: http://www.gravityforms.ir
+Requires at least: 3.5
+Tested up to: 4.1
+Stable tag: 1.3.1
+License: GPL 2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+This WordPress plugin extends the Gravity Forms plugin and its addons with the Persian language .
+
+== Description ==
+**Persian Gravity Forms**
+This plugin extends the Gravity Forms and its addons with Persian language
+
+= Features =
+* Translate Gravity Forms and its addons to Persian .
+* Add Iran provinces (states) in Fields , Forms and predefined choice .
+* Add Iranian Rial and Toman Currency .
+* Add Yekan Font to Gravity Forms and make it attractive .
+* Added Iranian National Code Field .
+* Make it RTL .
+* Add Private status option to post status .
+* Add Iranian payment gateways transaction id , transaction status and ... to Merge Tags .
+* and...
+
+= Persian Description =
+* ترجمه هسته افزونه و ادان های آن ...
+* اضافه کردن استان های ایران برای استفاده در فیلد های آدرس و ...
+* اضافه کردن واحد پولی ریال و تومان
+* زیبا سازی محیط کاربری به وسیله استایل دهی و ...
+* اضافه کردن فیلد کد ملی ایران ، این فیلد کد ملی نادرست را برطبق استانداردهای کشور تشخیص داده و از ثبت شدن آن جلوگیری میکند . همچنین این فیلد دارای تنظیمات پیشرفته تری نیز می باشد
+* راستچین سازی افزونه و صفحه پرینت و ادان ها و ...
+* اضافه کردن وضعیت پست خصوصی برای فیلد های ارسال پست .
+* اضافه کردن برچسب وضعیت تراکنش ، کد رهگیری و ... به برچسب های ایمیل و صفحه تاییدیه که این برچسب در خود افزونه وجود نداشت .
+* افزونه درگاه های ایرانی آن نیز به صورت جدا عرضه خواهد شد ....
+* قرار دادن کد رهگیری برای تمامی فرم ها
+* رسم نمودار شمسی برای درگاه های پرداخت
+* ....
+
+= Compatibility =
+* Wordpress 3.5
+* Gravity Forms 1.8.x
+
+= Support =
+*  [Persian Support in GravityForms.ir](http://GravityForms.ir/)
+
+== Installation ==
+1. Install and Activate Gravity Forms 1.8.x .
+2. Upload `persian-gravity-forms` folder to the `/wp-content/plugins/` directory
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Enjoy It :)
+
+== Frequently asked questions ==
+
+= Is Persian Gravity Forms compatible with future versions of gravity forms? =
+Also yes .
+
+= Where can I find more information and documentation about the plug-in? =
+You can read complete documentations on the [GravityForms.ir](http://www.GravityForms.ir) .
+
+
+== Screenshots ==
+
+01. Forms in wordpress dashboard.
+02. Forms list
+03. Adding new form
+04. Fields list
+05. Form setting page
+06. form confirmation actions
+07. form confirmation emails
+08. plugins setting page
+09. entries list
+10. entries detail
+11. coupons
+12. post fields
+13. front end.
+14. Iran states
+15. Iran states
+16. Iranian National Code field
+17. Iranian National Code field Standard setting
+18. Iranian National Code field in front-end
+19. Iranian National Code field Advanced setting
+
+== Changelog ==
+= 1.3.1 =
+* Fixed : Jquery Ui Jalali DatePicker 
+* Added Jalali Months to predefined choices
+* Added Gravity Form Sticky List Translate
+* Improve Transactions Security Levels
+* Source Code Cleaned up
+= 1.3.1 =
+* Fixed : Some Minor bugs
+= 1.2.0 =
+* Fixed : Date Field Validation
+= 1.1.5 =
+* Fixed : Some Minor bugs
+= 1.1.4 =
+* Fixed : Cannot modify header information...
+= 1.1.3 =
+* Fixed : Some Minor bugs
+= 1.1.2 =
+* Added transaction id for all forms
+* Added rtl merge tags for emails
+* Support custom page for callback url
+* Fixed some css bugs
+* Added wp_session class for more security
+* Added Jquery flot for transaction graph
+* Added Jalali Class for transaction graph
+= 1.1.1 =
+* Added Iranian National Code field
+* See Screenshots 16-19
+= 1.0.2 =
+* Fixed : Some Minor bugs
+= 1.0.1 =
+* Fixed : Some Minor bugs
+= 1.0.0 =
+* First version
+
+== Upgrade Notice ==
+= 1.3.1 =
+* Fixed : Jquery Ui Jalali DatePicker 
+* Added Jalali Months to predefined choices
+* Added Gravity Form Sticky List Translate
+* Improve Transactions Security Levels
+* Source Code Cleaned up
+= 1.3.1 =
+* Fixed : Some Minor bugs
+= 1.2.0 =
+* Fixed : Date Field Validation
+= 1.1.5 =
+* Fixed : Some Minor bugs
+= 1.1.4 =
+* Fixed : Cannot modify header information...
+= 1.1.3 =
+* Fixed : Some Minor bugs
+= 1.1.2 =
+* Added transaction id for all forms
+* Added rtl merge tags for emails
+* Support custom page for callback url
+* Fixed some css bugs
+* Added wp_session class for more security
+* Added Jquery flot for transaction graph
+* Added Jalali Class for transaction graph
+= 1.1.1 =
+* Added Iranian National Code field
+* See Screenshots 16-19
+= 1.0.2 =
+* Fixed : Some Minor bugs
+= 1.0.1 =
+* Fixed : Some Minor bugs
+= 1.0.0 =
+* First version
+
+==Traducciones ==
+You can read complete documentations on the [GravityForms.ir](http://www.GravityForms.ir)
